f1640a32428f5090570862026ec70896c82b8a0e
hashanp <10punchihewah@whsb.essex.sch.uk>
2014-07-29 09:55:43 +0000

    Added levels

---
+++ levels.js
@@ -0,0 +1,56 @@
+module.exports = {
+  1: {
+    minLength:3,
+    maxLength:5,
+    minCorpusCount:50000,
+    maxCorpusCount:55000
+  },
+  2: {
+    minLength:3,
+    maxLength:5,
+    minCorpusCount:45000,
+    maxCorpusCount:50000
+  },
+  3: {
+    minLength:5,
+    maxLength:7,
+    minCorpusCount:45000,
+    maxCorpusCount:50000
+  },
+  4: {
+    minLength:5,
+    maxLength:7,
+    minCorpusCount:40000,
+    maxCorpusCount:45000
+  },
+  5: {
+    minLength:7,
+    maxLength:9,
+    minCorpusCount:40000,
+    maxCorpusCount:45000
+  },
+  6: {
+    minLength:7,
+    maxLength:9,
+    minCorpusCount:35000,
+    maxCorpusCount:40000
+  },
+  7: {
+    minLength:9,
+    maxLength:12,
+    minCorpusCount:35000,
+    maxCorpusCount:40000
+  },
+  8: {
+    minLength:9,
+    maxLength:12,
+    minCorpusCount:25000,
+    maxCorpusCount:30000
+  },
+  9: {
+    minLength:12,
+    maxLength:15,
+    minCorpusCount:25000,
+    maxCorpusCount:30000
+  }
+};
